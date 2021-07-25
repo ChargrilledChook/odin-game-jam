72e679560e3596d2b72efa54dd86feeeebd84528
chook <dylanw212@gmail.com>
2021-07-25 15:36:02 +1000

    basic logic implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/Footer.css";
 
 function Footer() {
-  return <footer className="site-footer">Footer</footer>;
+  return (
+    <footer className="site-footer">
+      Made by two mad lads for the Odin Game Jam
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/MainBoard.css";
+import _ from "lodash";
+import wordList from "../words.js";
 
 function MainBoard() {
+  const [words, setWords] = useState(wordList.split("\n"));
+  const [word, setWord] = useState(_.sample(words));
+  const [answer, setAnswer] = useState("");
+  const [score, setScore] = useState(0);
+
+  const jumbledWord = _.shuffle(word.split("")).join(" ").toUpperCase();
+  const handleChange = (e) => {
+    return setAnswer(e.target.value);
+  };
+
+  const checkAnswer = () => {
+    console.log(word);
+    console.log(answer);
+    console.log(word === answer);
+    return word === answer;
+  };
+
+  const playRound = () => {
+    if (checkAnswer()) {
+      setScore(score + 1);
+    }
+  };
+
   return (
     <main className="main-board">
       <div className="board-header">
-        <div className="header-cell">Score: 4</div>
+        <div className="header-cell">Score: {score}</div>
         <div className="header-cell">Hint</div>
         <div className="header-cell">Grade: 5</div>
       </div>
 
       <div className="center-text">
-        <h2>Jumbled Text</h2>
-        <p className="hint-text"></p>
+        <h2 className="jumble">{jumbledWord}</h2>
+        <p className="hint-text">Text that is jumbled</p>
       </div>
 
       <div className="board-input">
-        <input className="main-input" type="text" />
+        <input className="main-input" type="text" onChange={handleChange} />
         <div className="button-container">
           <button>Pass</button>
-          <button>Enter</button>
+          <button onClick={() => playRound()}>Enter</button>
         </div>
       </div>
     </main>
