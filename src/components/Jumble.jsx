24a8490907d570228f90589e3f22548ec9849045
chook <dylanw212@gmail.com>
2021-07-26 12:48:08 +1000

    jumble component extracted and word no longer jumbles while entering letters

---
+++ src/components/Jumble.jsx
@@ -0,0 +1,22 @@
+import { shuffle } from "lodash";
+import { useEffect, useState } from "react";
+
+function Jumble({ word }) {
+  const [jumble, setJumble] = useState(
+    shuffle(word.split("")).join(" ").toUpperCase()
+  );
+
+  useEffect(
+    () => setJumble(shuffle(word.split("")).join(" ").toUpperCase()),
+    [word]
+  );
+
+  return (
+    <div className="center-text">
+      <h2 className="jumble">{jumble}</h2>
+      <p className="hint-text">Text that is jumbled</p>
+    </div>
+  );
+}
+
+export default Jumble;
--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,32 +1,22 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "../styles/MainBoard.css";
 import _ from "lodash";
 import wordList from "../words.js";
+import Jumble from "./Jumble";
 
 function MainBoard() {
   const [words, setWords] = useState(wordList.split("\n"));
   const [word, setWord] = useState(_.sample(words));
-  // const [jumble, setJumble] = _.shuffle(word.split("")).join(" ").toUpperCase();
   const [answer, setAnswer] = useState("");
   const [score, setScore] = useState(0);
-
-  // Find a better way to do this. May be easier with  multiple components
-  const jumbledWord = _.shuffle(word.split("")).join(" ").toUpperCase();
 
   const handleChange = (e) => {
     return setAnswer(e.target.value);
   };
 
-  const checkAnswer = () => {
-    console.log(word);
-    console.log(answer);
-    console.log(word === answer);
-    return word === answer;
-  };
-
   const playRound = (e) => {
     e.preventDefault();
-    if (checkAnswer()) {
+    if (word === answer) {
       setScore(score + 1);
     }
     // Keep an eye out if this allows a word to repeat a single time
@@ -46,10 +36,7 @@ function MainBoard() {
         <div className="header-cell">Grade: 5</div>
       </div>
 
-      <div className="center-text">
-        <h2 className="jumble">{jumbledWord}</h2>
-        <p className="hint-text">Text that is jumbled</p>
-      </div>
+      <Jumble word={word} />
 
       <form className="board-input">
         <input
