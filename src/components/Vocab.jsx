d07f3b2da73fd0730169feec0f4f7d4222e3d375
chook <dylanw212@gmail.com>
2021-07-30 11:23:55 +1000

    sort vocab alphabetically

--- src/components/Vocab.jsx
+++ src/components/Vocab.jsx
@@ -3,14 +3,16 @@ import { wordList } from "../words/newWords";
 function Vocab(props) {
   return (
     <main className="vocab">
-      {wordList.map((entry) => {
-        return (
-          <div>
-            <h2>{entry.name}</h2>
-            <p>{entry.definition}</p>
-          </div>
-        );
-      })}
+      {wordList
+        .sort((a, b) => a.word.localeCompare(b.word))
+        .map((entry) => {
+          return (
+            <div>
+              <h2>{entry.word}</h2>
+              <p>{entry.definition}</p>
+            </div>
+          );
+        })}
     </main>
   );
 }
