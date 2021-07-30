9f7705c75416a5f4d581ed794e82384544e82b3b
William <willabraham1@gmail.com>
2021-07-30 14:43:42 +1000

    Updated button font and added hint button but with no func at the moment

--- src/components/BoardHeader.jsx
+++ src/components/BoardHeader.jsx
@@ -4,7 +4,7 @@ function BoardHeader({ score }) {
   return (
     <div className="board-header">
       <div className="header-cell">Score: {score}</div>
-      <div className="header-cell">Hint</div>
+      <button className="hint">Hint</button>
       <div className="header-cell">Grade: 5</div>
     </div>
   );
