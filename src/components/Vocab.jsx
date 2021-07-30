9a88c0b9ea11d6216ca78755c09ac062165d3239
chook <dylanw212@gmail.com>
2021-07-30 12:40:02 +1000

    add page of vocab with definitions

--- src/components/Vocab.jsx
+++ src/components/Vocab.jsx
@@ -1,18 +1,26 @@
 import { wordList } from "../words/newWords";
+import "../styles/Vocab.css";
 
 function Vocab(props) {
   return (
-    <main className="vocab">
-      {wordList
-        .sort((a, b) => a.word.localeCompare(b.word))
-        .map((entry) => {
-          return (
-            <div>
-              <h2>{entry.word}</h2>
-              <p>{entry.definition}</p>
-            </div>
-          );
-        })}
+    <main>
+      <h1 className="page-title">Word List</h1>
+      <p className="page-blurb">
+        Short blurb about all the words in the game being here for study.
+        Probably need some nav links here too - can manually type for now
+      </p>
+      <div className="vocab-container">
+        {wordList
+          .sort((a, b) => a.word.localeCompare(b.word))
+          .map((entry) => {
+            return (
+              <div className="vocab-entry">
+                <h2 className="vocab-word">{entry.word}</h2>
+                <p className="vocab-def">{entry.definition}</p>
+              </div>
+            );
+          })}
+      </div>
     </main>
   );
 }
