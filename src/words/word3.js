1977aa6620f89aa6df85fbdd1c48d3d58d8db2a6
chook <dylanw212@gmail.com>
2021-07-30 12:49:07 +1000

    add new longer list of words with new grade field

---
+++ src/words/word3.js
@@ -0,0 +1,566 @@
+export const wordList = [
+  { word: "abhor", definition: "Hate", grade: "8" },
+  { word: "abrasive", definition: "Rough; coarse; harsh", grade: "8" },
+  { word: "bigot", definition: "Narrow-minded, prejudiced person", grade: "8" },
+  { word: "bilk", definition: "Cheat; defraud", grade: "8" },
+  { word: "counterfeit", definition: "Fake; false", grade: "8" },
+  { word: "covert", definition: "Hidden; undercover", grade: "8" },
+  { word: "enfranchise", definition: "Give voting rights", grade: "8" },
+  { word: "engender", definition: "Cause", grade: "8" },
+  { word: "hamper", definition: "Hinder; obstruct", grade: "8" },
+  {
+    word: "hangar",
+    definition: "Storage area (like garage) for a plane",
+    grade: "8",
+  },
+  { word: "kindle", definition: "To start a fire", grade: "8" },
+  { word: "knotty", definition: "Complex; difficult to solve", grade: "8" },
+  { word: "noxious", definition: "Harmful; poisonous; lethal", grade: "8" },
+  {
+    word: "nuance",
+    definition: "Something subtle; a fine shade of meaning",
+    grade: "8",
+  },
+  { word: "placid", definition: "Calm; peaceful", grade: "8" },
+  {
+    word: "plagiarism",
+    definition: "Taking credit for someone else's writing or ideas",
+    grade: "8",
+  },
+  { word: "remuneration", definition: "Payment for work done", grade: "8" },
+  { word: "renown", definition: "Fame", grade: "8" },
+  { word: "talisman", definition: "Lucky charm", grade: "8" },
+  { word: "tangent", definition: "Going off the main subject", grade: "8" },
+
+  {
+    word: "adjacent",
+    definition: "Neighboring, nearby, adjoining, bordering",
+    grade: "8",
+  },
+  {
+    word: "conventions",
+    definition: "Meeting, gathering, rule, agreement",
+    grade: "8",
+  },
+  {
+    word: "configuration",
+    definition: "Pattern, design, arrangement",
+    grade: "8",
+  },
+  { word: "derive", definition: "Origin, root, source, beginning", grade: "8" },
+  {
+    word: "elaboration",
+    definition:
+      "Intensification, strengthening, further explanation, further details",
+    grade: "8",
+  },
+  {
+    word: "etymology",
+    definition: "Derivation of a word, history of a word",
+    grade: "8",
+  },
+  { word: "explicit", definition: "Open, clear, precise, exact", grade: "8" },
+  {
+    word: "implicit",
+    definition: "Understood, unreserved, absolute",
+    grade: "8",
+  },
+  {
+    word: "facilitate",
+    definition: "Make easy, make possible, aid, assist",
+    grade: "8",
+  },
+  {
+    word: "idioms",
+    definition: "Phrase, saying, expression, jargon",
+    grade: "8",
+  },
+  {
+    word: "literal",
+    definition: "Factual, truthful, exact, accurate, figurative",
+    grade: "8",
+  },
+  {
+    word: "figurative",
+    definition: "Symbolic, metaphorical, abstract",
+    grade: "8",
+  },
+  {
+    word: "imperative",
+    definition:
+      "Very important, vital, crucial, authoritative, requirement, law",
+    grade: "8",
+  },
+  {
+    word: "juxtaposition",
+    definition: "Combination, coincidence, union",
+    grade: "8",
+  },
+  { word: "proverb", definition: "Maxim, axiom, saying, motto", grade: "8" },
+  {
+    word: "subliminal",
+    definition: "Subconscious hidden unconscious",
+    grade: "8",
+  },
+  {
+    word: "superlative",
+    definition: "Excellent, unmatched, unbeatable, outstanding",
+    grade: "8",
+  },
+  {
+    word: "syntax",
+    definition: "Grammar, sentence structure, language rules",
+    grade: "8",
+  },
+  { word: "vernacular", definition: "Dialect, language, lingo", grade: "8" },
+  {
+    word: "consolidate",
+    definition: "Merge, combine, strengthen, secure",
+    grade: "8",
+  },
+  {
+    word: "coherent",
+    definition: "Logical, rational, sound, consistent",
+    grade: "8",
+  },
+  {
+    word: "rationale",
+    definition: "Underlying principle, basis, foundation, motivation",
+    grade: "8",
+  },
+  { word: "circumvent", definition: "Avoid, get around, evade", grade: "8" },
+  { word: "circumference", definition: "Perimeter, boundary", grade: "8" },
+  {
+    word: "consecutive",
+    definition: "Successive, following, repeated",
+    grade: "8",
+  },
+  {
+    word: "integral",
+    definition:
+      "Essential, vital, important, basic, connected, at the heart of",
+    grade: "8",
+  },
+  {
+    word: "disseminate",
+    definition: "Distribute, broadcast, circulate, spread",
+    grade: "8",
+  },
+  {
+    word: "phenomena",
+    definition:
+      "Occurrence, fact, experience, marvel, wonder, exceptional person",
+    grade: "8",
+  },
+  {
+    word: "apparent",
+    definition: "Obvious, clear, evident, seeming, deceptive",
+    grade: "8",
+  },
+  {
+    word: "correlate",
+    definition: "Associate, compare, link, show a relationship",
+    grade: "8",
+  },
+  { word: "intonation", definition: "Accent, inflection, tone", grade: "8" },
+  {
+    word: "intervene",
+    definition: "Arbitrate, get involved, occur, take place ",
+    grade: "8",
+  },
+  { word: "intervention", definition: "Interference", grade: "8" },
+  {
+    word: "imposed",
+    definition: "Imposition, burden, nuisance, obligation",
+    grade: "8",
+  },
+  { word: "prohibit", definition: "Forbid, exclude, make illegal", grade: "8" },
+  { word: "prohibition", definition: "Ban, exclusion", grade: "8" },
+  {
+    word: "proposal",
+    definition: "Suggestion, offer, application ",
+    grade: "8",
+  },
+  { word: "proposition", definition: "Proposal, plan, scheme", grade: "8" },
+  {
+    word: "provision",
+    definition: "Stipulation, condition, terms, specification",
+    grade: "8",
+  },
+  {
+    word: "fundamental",
+    definition: "Basic, primary, essential, important, vital",
+    grade: "8",
+  },
+  {
+    word: "episode",
+    definition: "Incidence, installment, event, occurrence ",
+    grade: "8",
+  },
+  { word: "episodic", definition: "Periodic", grade: "8" },
+  {
+    word: "recitation",
+    definition: "Recital, reading, performance, narration",
+    grade: "8",
+  },
+  {
+    word: "connotation",
+    definition: "Mean, predict, imply, suggest, implication, undertone",
+    grade: "8",
+  },
+  {
+    word: "extraneous",
+    definition: "Irrelevant, unrelated, unconnected, not on the subject",
+    grade: "8",
+  },
+  {
+    word: "conscious",
+    definition: "Aware, mindful, awake, deliberate ",
+    grade: "8",
+  },
+  {
+    word: "subconscious",
+    definition: "Subliminal, hidden, unintentional, involuntary",
+    grade: "8",
+  },
+  {
+    word: "authentic",
+    definition: "Genuine, real, valid, true, reliable, realistic",
+    grade: "8",
+  },
+
+  { word: "acquire", definition: "Obtain, get gain ", grade: "7" },
+  {
+    word: "acquisition",
+    definition: "Achievement, purchase, possession, asset",
+    grade: "7",
+  },
+  {
+    word: "argument",
+    definition: "Quarrel, disagreement, case, reasoning, contention",
+    grade: "7",
+  },
+  {
+    word: "assumption",
+    definition: "Statement, guess, theory, belief",
+    grade: "7",
+  },
+  {
+    word: "coordinate",
+    definition: "Organize, direct, manage, bring together",
+    grade: "7",
+  },
+  { word: "dialect", definition: "Language, talk, idiom", grade: "7" },
+  {
+    word: "editorial",
+    definition: "Viewpoint, perspective, position, point of view",
+    grade: "7",
+  },
+  {
+    word: "etiquette",
+    definition: "Manners, good manners, protocol",
+    grade: "7",
+  },
+  { word: "expository", definition: "Explanation, account", grade: "7" },
+  { word: "figurative", definition: "Metaphoric, abstract", grade: "7" },
+  {
+    word: "literal",
+    definition: "Factual, truthful, exact, word by word, verbatim",
+    grade: "7",
+  },
+  {
+    word: "inconsistency",
+    definition:
+      "Not in agreement, conflicting, unpredictable, shifting, unreliable, erratic",
+    grade: "7",
+  },
+  { word: "jargon", definition: "Terminology, slang, lingo", grade: "7" },
+  { word: "modify", definition: "Alteration, change, adjustment", grade: "7" },
+  {
+    word: "projection",
+    definition: "Outcrop, bulge, forecast, prediction",
+    grade: "7",
+  },
+  {
+    word: "slant",
+    definition: "Angle, viewpoint, attitude, point of view",
+    grade: "7",
+  },
+  {
+    word: "slang",
+    definition: "Informal usage, playful language or idioms",
+    grade: "7",
+  },
+  { word: "stereotype", definition: "Typecast, label", grade: "7" },
+  {
+    word: "subordinate",
+    definition: "Secondary, lesser, inferior, assistant, junior, underling",
+    grade: "7",
+  },
+  {
+    word: "transition",
+    definition: "Change, evolution, shift, alteration, transfer",
+    grade: "7",
+  },
+  { word: "inflection", definition: "Nuance, variation, variety", grade: "7" },
+  {
+    word: "collaboration",
+    definition: "Teamwork, partnership, group, effort",
+    grade: "7",
+  },
+  {
+    word: "clarify",
+    definition:
+      "Make clear, explain, spell out, simplify, refine, purify, filter",
+    grade: "7",
+  },
+  {
+    word: "exponent",
+    definition: "Advocate, supporter, example, model, interpreter, explainer",
+    grade: "7",
+  },
+  {
+    word: "ratio",
+    definition: "Relation, relative amount, percentage",
+    grade: "7",
+  },
+  {
+    word: "proportion",
+    definition: "Amount, quantity, ratio, comparison, relationship",
+    grade: "7",
+  },
+  {
+    word: "discord",
+    definition: "Disagreement, conflict, dispute, disharmony, friction",
+    grade: "7",
+  },
+  {
+    word: "adapt",
+    definition: "Version, edition, alteration, change",
+    grade: "7",
+  },
+  {
+    word: "approximate",
+    definition: "Estimated, fairly accurate, rough, near, ballpark",
+    grade: "7",
+  },
+  {
+    word: "transformation",
+    definition: "Alteration, change, conversion, renovation",
+    grade: "7",
+  },
+  {
+    word: "composition",
+    definition: "Create, write, arrange, calm, settle down",
+    grade: "7",
+  },
+  { word: "objection", definition: "Opposition, protest, doubt", grade: "7" },
+  {
+    word: "supported",
+    definition: "Hold up, maintain, foundation, encourage, assist, backing",
+    grade: "7",
+  },
+  { word: "sequence", definition: "Chronological, in order", grade: "7" },
+  {
+    word: "maintain",
+    definition: "Uphold, preserve, argue, claim, look after, care for",
+    grade: "7",
+  },
+  {
+    word: "promote",
+    definition: "Endorse, encourage, advance, further advertise, campaign for",
+    grade: "7",
+  },
+  {
+    word: "extensive",
+    definition: "Widespread, wide-ranging, big, vast",
+    grade: "7",
+  },
+  {
+    word: "formal",
+    definition: "Procedure, requirement, reserve, stiffness",
+    grade: "7",
+  },
+
+  {
+    word: "sketch",
+    definition: "Preliminary drawing, brief account, short presentation",
+    grade: "6",
+  },
+  {
+    word: "broadcast",
+    definition: "Transmit, show, publicize, make known, advertise",
+    grade: "6",
+  },
+  {
+    word: "climax",
+    definition: "Peak, high point, pinnacle, best moment",
+    grade: "6",
+  },
+  { word: "era", definition: "Age, epoch, period", grade: "6" },
+  {
+    word: "dialogue",
+    definition: "Conversation, interview, exchange of ideas, discussion",
+    grade: "6",
+  },
+  {
+    word: "feedback",
+    definition: "Criticism, advice, reaction, view",
+    grade: "6",
+  },
+  {
+    word: "interjection",
+    definition: "Exclamation, interruption, speaking",
+    grade: "6",
+  },
+  {
+    word: "mechanics",
+    definition: "Workings, technicalities, procedures",
+    grade: "6",
+  },
+  {
+    word: "media",
+    definition: "Channels of general communication, information",
+    grade: "6",
+  },
+  {
+    word: "plagiarism",
+    definition: "Copying, stealing, illegal use, breach of copyright",
+    grade: "6",
+  },
+  {
+    word: "recurring",
+    definition: "Chronic, returning, frequent, habitual",
+    grade: "6",
+  },
+  {
+    word: "representation",
+    definition: "Image, depiction, illustration, account, statement",
+    grade: "6",
+  },
+  {
+    word: "stress",
+    definition: "Pressure, strain, emphasis, importance, underline, underscore",
+    grade: "6",
+  },
+  { word: "audience", definition: "Spectators, viewers", grade: "6" },
+  {
+    word: "technology",
+    definition: "Knowledge, expertise, equipment, machinery",
+    grade: "6",
+  },
+  {
+    word: "text",
+    definition: "Book, manuscript, transcript, textbook",
+    grade: "6",
+  },
+  {
+    word: "origin",
+    definition: "Source, derivation, cause, beginning",
+    grade: "6",
+  },
+  {
+    word: "irrelevant",
+    definition: "Immaterial, neither here nor there, unrelated, inappropriate",
+    grade: "6",
+  },
+  {
+    word: "generalization",
+    definition: "Sweeping statement, simplification, oversimplification",
+    grade: "6",
+  },
+  {
+    word: "exaggerate",
+    definition: "Overstate, embellish, amplify, inflate",
+    grade: "6",
+  },
+  {
+    word: "substitution",
+    definition: "Replacement, switch, exchange, changeover",
+    grade: "6",
+  },
+  {
+    word: "invalid",
+    definition: "Unacceptable, unsound, untrue, null, void",
+    grade: "6",
+  },
+  {
+    word: "terminate",
+    definition: "End, finish, come to an end, conclude, stop, cease",
+    grade: "6",
+  },
+  {
+    word: "distribution",
+    definition: "Sharing, giving out, delivery, supply, spreading",
+    grade: "6",
+  },
+  { word: "median", definition: "Middle, center, norm", grade: "6" },
+  {
+    word: "mode",
+    definition: "Form, style, manner, method, means",
+    grade: "6",
+  },
+  {
+    word: "range",
+    definition: "Variety, choice, series, vary, reach, scope, span",
+    grade: "6",
+  },
+  {
+    word: "primary",
+    definition: "Main, chief, most important, first, leading",
+    grade: "6",
+  },
+  {
+    word: "immune",
+    definition:
+      "Resistant, protected, invulnerable, unaffected, safe, exempt, excepted",
+    grade: "6",
+  },
+  { word: "dowry", definition: "Gift, offering, grant", grade: "6" },
+  {
+    word: "conduct",
+    definition: "Behavior, demeanor, carry out, perform, management, handling",
+    grade: "6",
+  },
+  {
+    word: "convert",
+    definition: "Change, exchange, switch, win over, bring round, convince",
+    grade: "6",
+  },
+  {
+    word: "values",
+    definition: "Principles, standards, morals, ethics, ideals",
+    grade: "6",
+  },
+  {
+    word: "interact",
+    definition: "Interrelate, act together, work together, cooperate, network",
+    grade: "6",
+  },
+  {
+    word: "similarity",
+    definition: "Resemblance, comparison, likeness, match",
+    grade: "6",
+  },
+  {
+    word: "event",
+    definition: "Occasion, happening, incident, result, consequence, outcome",
+    grade: "6",
+  },
+  {
+    word: "personify",
+    definition: "Typify, characterize, show, humanize, give a human face to",
+    grade: "6",
+  },
+  {
+    word: "contrast",
+    definition: "Evaluate, match up, to associate, relate, link",
+    grade: "6",
+  },
+  {
+    word: "benefit",
+    definition:
+      "Advantage, profit, help, promote, do good, to gain, allowance, subsidy",
+    grade: "6",
+  },
+];
