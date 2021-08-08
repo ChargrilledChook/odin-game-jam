7a5ede0dea25f312ef55760aaa97414a829cf7dd
chook <dylanw212@gmail.com>
2021-08-08 13:05:33 +1000

    change browser router to hash router

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Title from "./components/Title";
 import Vocab from "./components/Vocab";
@@ -7,14 +7,14 @@ import Levels from "./components/Levels";
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/vocab" component={Vocab} />
         <Route path="/levels" component={Levels} />
         <Route path="/play" component={Layout} />
         <Route path="/" component={Title} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
