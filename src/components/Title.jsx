6fd34b58dbfd3376043562597ce2d757be0897cf
chook <dylanw212@gmail.com>
2021-08-08 11:25:11 +1000

    add logo and padding to title screen

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,14 @@
 import "../styles/Title.css";
 import "../styles/generalStyles.css";
 import { Link } from "react-router-dom";
+import logo from "../images/word-jamble.png";
 
 function Title() {
   return (
     <main className="title-screen">
       <h1>Word Jamble</h1>
+      <img className="logo" src={logo} alt="the site logo" />
       <p>Unjamble the words and improve your vocabulary!</p>
-
       <Link to="/play">
         {" "}
         <button className="button">Play</button>
