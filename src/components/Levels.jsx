f91f5fa4ac3d6d3511ddc3ccb17798ae7ce4ada9
chook <dylanw212@gmail.com>
2021-08-08 11:43:50 +1000

    make smaller

--- src/components/Levels.jsx
+++ src/components/Levels.jsx
@@ -4,13 +4,9 @@ import "../styles/generalStyles.css";
 function Levels(props) {
   return (
     <main className="level-select">
-      <div className="button">Grade 1</div>
-      <div className="button">Grade 2</div>
-      <div className="button">Grade 3</div>
-      <div className="button">Grade 4</div>
-      <div className="button">Grade 5</div>
-      <div className="button">Grade 6</div>
-      <div className="button">Grade 7</div>
+      <div className="button">Easy</div>
+      <div className="button">Medium</div>
+      <div className="button">Hard</div>
       <div className="button">All</div>
     </main>
   );
