ce2eb6f5a54557c45b12202a06c598052ff7177f
chook <dylanw212@gmail.com>
2021-08-08 13:07:56 +1000

    restructure hash router

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Title from "./components/Title";
 import Vocab from "./components/Vocab";
@@ -7,14 +7,14 @@ import Levels from "./components/Levels";
 
 const Routes = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path="/vocab" component={Vocab} />
         <Route path="/levels" component={Levels} />
         <Route path="/play" component={Layout} />
         <Route path="/" component={Title} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <HashRouter>
+      <Routes />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
