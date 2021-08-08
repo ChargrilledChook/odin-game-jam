26e325af3a18ea2bc59cde8e233c38a38f8cc2c1
chook <dylanw212@gmail.com>
2021-08-08 12:14:44 +1000

    change grade to round number

--- src/components/BoardHeader.jsx
+++ src/components/BoardHeader.jsx
@@ -1,13 +1,14 @@
 import "../styles/BoardHeader.css";
 
-function BoardHeader({ score, grade, toggleHint }) {
+function BoardHeader({ score, round, toggleHint }) {
+  const displayRound = () => (round > 20 ? 20 : round);
   return (
     <div className="board-header">
       <div className="header-cell">Score: {score}</div>
       <button onClick={() => toggleHint()} className="button">
         Hint
       </button>
-      <div className="header-cell">Grade: {grade}</div>
+      <div className="header-cell">Word: {displayRound()} / 20</div>
     </div>
   );
 }
